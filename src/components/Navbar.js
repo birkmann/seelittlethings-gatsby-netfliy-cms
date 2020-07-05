fd6fdf32fdfe15d1e3a7673ffbad6d916e53bcdb
Rafael Birkmann <mail@rafaelbirkmann.de>
2020-07-05 18:28:00 +0200

    layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React from "react";
+import { Link } from "gatsby";
+import github from "../img/github-icon.svg";
+import logo from "../img/logo.svg";
 
 const Navbar = class extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       active: false,
-      navBarActiveClass: '',
-    }
+      navBarActiveClass: "",
+    };
   }
 
   toggleHamburger = () => {
@@ -23,60 +23,60 @@ const Navbar = class extends React.Component {
         // set the class in state for the navbar accordingly
         this.state.active
           ? this.setState({
-              navBarActiveClass: 'is-active',
+              navBarActiveClass: "is-active",
             })
           : this.setState({
-              navBarActiveClass: '',
-            })
+              navBarActiveClass: "",
+            });
       }
-    )
-  }
+    );
+  };
 
   render() {
     return (
-      <header className='main'>
-        <div className='container'>
-          <Link to='/' className='logo' title='Logo'>
-            <div className='icon'>
-              <img src={logo} alt='Logo' />
+      <header className="main">
+        <div className="container">
+          <Link to="/" className="logo" title="Logo">
+            <div className="icon">
+              <img src={logo} alt="Logo" />
             </div>
-            <div className='text'>
+            <div className="text">
               see<span>littlethings</span>
             </div>
           </Link>
-          <div className='right'>
-            <nav className='top'>
+          <div className="nav-wrapper">
+            <nav className="top">
               <ul className={`navbar-menu ${this.state.navBarActiveClass}`}>
                 <li>
-                  <Link className='navbar-item' to='/blog'>
+                  <Link className="navbar-item" to="/blog">
                     Blog
                   </Link>
                 </li>
                 <li>
-                  <Link className='navbar-item' to='/rezepte'>
+                  <Link className="navbar-item" to="/rezepte">
                     Rezepte
                   </Link>
                 </li>
                 <li>
-                  <Link className='navbar-item' to='/reisen'>
+                  <Link className="navbar-item" to="/reisen">
                     Reisen
                   </Link>
                 </li>
                 <li>
-                  <Link className='navbar-item' to='/lifestyle'>
+                  <Link className="navbar-item" to="/lifestyle">
                     Lifestyle
                   </Link>
                 </li>
               </ul>
             </nav>
-            <Link className='btn' to='/contact'>
+            <Link className="btn" to="/contact">
               Kontakt
             </Link>
           </div>
         </div>
       </header>
-    )
+    );
   }
-}
+};
 
-export default Navbar
+export default Navbar;
